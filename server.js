fe3c3c44f34f397bf771a5c7681f7b858dd5b61c
AbdullahDsouky220 <abdallahdsoukyabdelnabyhassen30_sd@science.bsu.edu.eg>
2023-08-05 22:29:11 +0300

    adding envorimental variables;

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+const dotenv = require('dotenv')
 const express=require('express')
 const app=express()
-
+dotenv.config({
+    path:'config.env'
+})
 
 app.get('/',(req,res)=>{
     res.send('hello world')
 })
 
-app.listen(3000,()=>{
-    console.log('the app is running on port 3000')
+app.listen(process.env.PORT,()=>{
+    console.log('the app is running on port '+process.env.PORT)
 })
