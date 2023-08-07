0473d62000eddd73f87a940e3f9fa4b06b9198ee
AbdullahDsouky220 <abdallahdsoukyabdelnabyhassen30_sd@science.bsu.edu.eg>
2023-08-07 11:11:13 +0300

    adding mongoose and create a model to create cateogry for testing

--- server.js
+++ server.js
@@ -1,28 +1,58 @@
-const dotenv = require('dotenv')
-const express=require('express')
-const morgan = require('morgan')
-const mongoose =require('mongoose')
-const app=express()
+const dotenv = require("dotenv");
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const app = express();
 
 //config
 dotenv.config({
-    path:'config.env'
-})
-
-//mongoose
-mongoose.connect(process.env.DATABASE_URL).then(
-    ()=>{
-        console.log(`the app has been connected to db successfully ${process.env.PORT}`)
-    }).catch((error)=>{
-    console.error(`there is an error in the app ${error}`);
+  path: "config.env",
 });
-//midllwares
-app.use(morgan)
-//routes
-app.get('/',(req,res)=>{
-    res.send('hello ')
-})
 
-app.listen(process.env.PORT,()=>{
-    console.log('the app is running on port '+process.env.PORT)
-})
+//midllwares
+app.use(morgan("dev"));
+app.use(express.json());
+//mongoose
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    console.log(
+      `the app has been connected to db successfully ${process.env.PORT}`
+    );
+  })
+  .catch((error) => {
+    console.error(`there is an error in the app ${error}`);
+  });
+
+//schema
+const CateogrySchema = new mongoose.Schema({
+  name: String,
+});
+const CateogryModel = mongoose.model("Cateogry", CateogrySchema);
+
+//routes
+//get /
+app.get("/", (req, res) => {
+  res.json("hello ");
+});
+//post /createCateogry
+
+app.post("/", (req, res) => {
+  const name = req.body.name;
+  const newCateogry = new CateogryModel({ name });
+  newCateogry.save()
+    .then((doc) => {
+      console.log(
+        `the cateogry has been created successfull ,congrates 🎊🎉🎉🎉`
+      );
+      res.json(doc);
+    })
+    .catch((error) => {
+      console.log(error, "infortunately there is an error dude 😢😢");
+      res.send(error)
+    });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("the app is running on port " + process.env.PORT);
+});
