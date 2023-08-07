51d94b237acca8a642c1fe513c82b2834a7e8d50
AbdullahDsouky220 <abdallahdsoukyabdelnabyhassen30_sd@science.bsu.edu.eg>
2023-08-07 23:45:48 +0300

    adding the service of getting all the categries and add route desc access for each route created

--- services/categoryService.js
+++ services/categoryService.js
@@ -1,10 +1,24 @@
 const { default: slugify } = require("slugify");
 const CategoryModel = require("../models/categriesModel");
 const expressAsyncHandler = require("express-async-handler");
+//@route GET /api/v1/category
+//@desc for getting all the list of categries created
+//@access public
+exports.getCategory = expressAsyncHandler(
+  async(req, res) => {
+    const allCategories=await CategoryModel.find({}
+      )
+    res.status(200).json({
+      results:allCategories.length,
+      data:allCategories
+    });
+  }
+)
 
-exports.getCategory = (req, res) => {
-  res.json("hello ");
-};
+
+//@route POST /api/v1/category
+//@desc this route is for creating a category 
+//@access private
 exports.createCategory = expressAsyncHandler(async(req, res) => {
   const name = req.body.name;
  const newCategory= await CategoryModel.create({ name, slug: slugify(name) })
