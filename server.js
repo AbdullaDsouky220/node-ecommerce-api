c912d85dbe181fb65fb797b5a131263e416c564b
AbdullahDsouky220 <abdallahdsoukyabdelnabyhassen30_sd@science.bsu.edu.eg>
2023-08-07 07:16:35 +0300

    adding mogoose for connection to the db

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const dotenv = require('dotenv')
 const express=require('express')
+const morgan = require('morgan')
+const mongoose =require('mongoose')
 const app=express()
+
+//config
 dotenv.config({
     path:'config.env'
 })
 
+//mongoose
+mongoose.connect(process.env.DATABASE_URL).then(
+    ()=>{
+        console.log(`the app has been connected to db successfully ${process.env.PORT}`)
+    }).catch((error)=>{
+    console.error(`there is an error in the app ${error}`);
+});
+//midllwares
+app.use(morgan)
+//routes
 app.get('/',(req,res)=>{
     res.send('hello ')
 })
